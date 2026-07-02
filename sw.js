2ddef5be0e02cdd1fe9010b916d8db8771fb28f3
vvishalsonaawane <vishals9008@gmail.com>
2026-07-02 18:55:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,10 @@
-// ChatCode Service Worker - enables "Add to Home Screen" installability
-const CACHE_NAME = 'chatcode-v3';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+// ChatCode Service Worker v4 - minimal, no caching
+const CACHE_NAME = 'chatcode-v4';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)).catch(() => {})
-  );
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((names) => {
-      return Promise.all(
-        names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
-      );
-    })
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const url = event.request.url;
-  const isShellFile = urlsToCache.some(f => url.endsWith(f.replace('./', '')));
-
-  if (isShellFile) {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  }
 });
